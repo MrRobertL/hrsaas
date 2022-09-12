3181e23853b93483b8be180e444698e2f8fd3543
MrRobert <308703907@qq.com>
2022-09-12 20:47:56 +0800

    人力资源项目初始化

---
+++ src/store/getters.js
@@ -0,0 +1,12 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  device: state => state.app.device
+}
+export default getters
+
+/*   visitedViews: state => state.tagsView.visitedViews,
+  cachedViews: state => state.tagsView.cachedViews,
+  introduction: state => state.user.introduction,
+  roles: state => state.user.roles,
+  permission_routes: state => state.permission.routes,
+  errorLogs: state => state.errorLog.logs */
